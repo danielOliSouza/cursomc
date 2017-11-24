8f8df2c691e86a414e8f3ef061eb83fa8bf9e322
Daniel Oliveira Souza <daniel.souza@tron.com.br>
2017-11-24 11:31:01 -0200

    Testando Domain - Categoria

--- src/main/java/br/com/daniel/cursomc/CursomcApplication.java
+++ src/main/java/br/com/daniel/cursomc/CursomcApplication.java
@@ -1,10 +1,12 @@
 package br.com.daniel.cursomc;
 
+import java.io.Serializable;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements Serializable{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
